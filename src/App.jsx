96e8cfb599f7d0fa6252f349d9386e6bf5f24130
Dennis-ux-ai <dennissabu444@gmail.com>
2025-07-22 17:18:29 +0530

    router changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
 import Home from './pages/Home/Home'
+import {Routes,Route} from 'react-router-dom'
 import Login from './pages/Login/Login'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
+
+
+
+
 
 const App = () => {
   return (
-    <Router>
-      <div className='app'>
-        <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='/home' element={<Home />} />
-        </Routes>
-        <ToastContainer />
-      </div>
-    </Router>
+    <div className='app'>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/home' element={<Home />} />
+      </Routes>
+      <ToastContainer />
+    </div>
   )
 }
 
