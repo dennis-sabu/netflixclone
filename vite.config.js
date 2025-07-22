a7718c41ee9ec065aa6975f2dc5d1e0fa4de3e80
Dennis-ux-ai <dennissabu444@gmail.com>
2025-07-22 18:38:51 +0530

    vite api

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env.API_KEY': JSON.stringify(import.meta.env.API_KEY || ''),
-    'process.env.AUTH_DOMAIN': JSON.stringify(import.meta.env.AUTH_DOMAIN || ''),
-    'process.env.PROJECT_ID': JSON.stringify(import.meta.env.PROJECT_ID || ''),
-    'process.env.STORAGE_BUCKET': JSON.stringify(import.meta.env.STORAGE_BUCKET || ''),
-    'process.env.MESSAGING_SENDER_ID': JSON.stringify(import.meta.env.MESSAGING_SENDER_ID || ''),
-    'process.env.APP_ID': JSON.stringify(import.meta.env.APP_ID || ''),
   }
 })
