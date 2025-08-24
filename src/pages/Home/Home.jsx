74c8ce7a90ba91ea0e0d49674e8b3c53e5787fb1
Dennis-ux-ai <dennissabu444@gmail.com>
2025-08-25 00:17:23 +0530

    Refactor Home component for improved readability and structure

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,45 +1,58 @@
-import React from 'react'
-import './Home.css'
+import React, { useEffect, useState } from 'react';
+import './Home.css';
 import NavBar from '../../components/NavBar/NavBar';
-import hero_banner from  '../../assets/hero_banner.jpg' // Assuming you have a hero banner image in assets
-import hero_title from '../../assets/hero_title.png' // Assuming you have a hero title image in assets
-import play_icon from '../../assets/play_icon.png' // Assuming you have a play icon image in assets
-import info_icon from '../../assets/info_icon.png' // Assuming you have an info icon image in
-import TitleCards from '../../components/TitleCards/TitleCards'
-import Footer from '../../components/Footer/Footer'
-
-
-
-
-
+import hero_banner from '../../assets/hero_banner.jpg';
+import hero_title from '../../assets/hero_title.png';
+import play_icon from '../../assets/play_icon.png';
+import info_icon from '../../assets/info_icon.png';
+import TitleCards from '../../components/TitleCards/TitleCards';
+import Footer from '../../components/Footer/Footer';
+import { auth } from '../../components/firebase';
 
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((currentUser) => {
+      if (!currentUser) {
+        window.location.href = '/login';
+      }
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) {
+    return <div style={{ color: 'white', textAlign: 'center', marginTop: '50px' }}>Loading...</div>;
+  }
+
   return (
     <div className='home'>
       <NavBar />
       <div className='hero'>
-       <img src={hero_banner} alt="Hero Banner" className='banner-img' />
-       <div className='hero-caption'> 
-        <img src={hero_title} alt="Hero Title" className='caption-img' />
-        <p>Discover his ties to a secret ancient order,  a young man living in modern istanbul embarks on  a quest to save the city from a immortal enemy.</p>
-        <div className="hero-btns">
-          <button className='btn'><img src={play_icon} alt="Play" />Play</button>
-          <button className='btn dark-btn'><img src={info_icon} alt="Info" />More Info</button>
+        <img src={hero_banner} alt="Hero Banner" className='banner-img' />
+        <div className='hero-caption'>
+          <img src={hero_title} alt="Hero Title" className='caption-img' />
+          <p>
+            Discover his ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy.
+          </p>
+          <div className="hero-btns">
+            <button className='btn'><img src={play_icon} alt="Play" />Play</button>
+            <button className='btn dark-btn'><img src={info_icon} alt="Info" />More Info</button>
+          </div>
+          <TitleCards />
         </div>
-        <TitleCards />
-       </div>
       </div>
       <div className="more-cards">
         <TitleCards title={"Blockbusters"} />
         <TitleCards title={"Only on Netflix"} />
         <TitleCards title={"Upcoming"} />
         <TitleCards title={"Top Picks for you"} />
-       
       </div>
       <Footer />
-     
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
